ea34e79cd108522a1740ebe3810592047835e6e9
mas7667 <fily.sara.keita@gmail.com>
2026-07-23 22:17:49 -0400

    Adding prometheus and grafana for monitoring

---
+++ src/main/java/com/gestioncompte/gestion_compte/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.gestioncompte.gestion_compte.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/actuator/**", "/h2-console/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .headers(headers -> headers.frameOptions(frameOptions -> frameOptions.disable()));
+
+        return http.build();
+    }
+}

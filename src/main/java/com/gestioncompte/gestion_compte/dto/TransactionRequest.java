6a0471a767e509530c7ee29f2e84f93b1387b101
mas7667 <fily.sara.keita@gmail.com>
2026-07-29 17:48:44 -0400

    Transaction request

--- src/main/java/com/gestioncompte/gestion_compte/dto/TransactionRequest.java
+++ src/main/java/com/gestioncompte/gestion_compte/dto/TransactionRequest.java
@@ -1,4 +1,44 @@
 package com.gestioncompte.gestion_compte.dto;
 
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import java.math.BigDecimal;
+
 public class TransactionRequest {
+
+    @NotBlank
+    private String accountNumber;
+
+    @NotNull
+    @DecimalMin(value = "0.01")
+    private BigDecimal amount;
+
+    // Used only for transfers
+    private String destinationAccountNumber;
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public String getDestinationAccountNumber() {
+        return destinationAccountNumber;
+    }
+
+    public void setDestinationAccountNumber(String destinationAccountNumber) {
+        this.destinationAccountNumber = destinationAccountNumber;
+    }
 }
